3b75001f32f0d6136d8a0c43d989a52ed199f0b8
Althran <overlord106@gmail.com>
2022-07-30 16:07:23 +0300

    Complete most_common

--- tasks/hard/most_common_and_longest.py
+++ tasks/hard/most_common_and_longest.py
@@ -7,11 +7,15 @@
 можно решить с помощью циклов и переменных, но предпочтительней с
 помощью модуля collections, используя collections.Counter
 """
+from collections import Counter
 
 
 def common_and_longest(text: str) -> tuple:
     common = None
     longest = None
+    text1 = text.split()
+    common = (Counter(text1).most_common(1)[0][0])
+    longest = max(text1, key=len)
     return common, longest
 
 
