2bf15d87cfcd3da8170b6b266ae683d040860ea9
Althran <overlord106@gmail.com>
2022-07-30 13:23:19 +0300

    buble_sort

--- tasks/hard/bubble_sort.py
+++ tasks/hard/bubble_sort.py
@@ -29,7 +29,11 @@
 
 
 def bubble_sort(array: list) -> list:
-    return None
+    for i in range(len(array) - 1):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
 
 
 if __name__ == '__main__':
