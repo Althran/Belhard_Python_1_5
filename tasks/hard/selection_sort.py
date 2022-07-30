520b00b8fe1fdf3b61a407b89fd58c2b5373b397
Althran <overlord106@gmail.com>
2022-07-30 13:37:51 +0300

    selection_sort

--- tasks/hard/selection_sort.py
+++ tasks/hard/selection_sort.py
@@ -19,6 +19,12 @@
 
 
 def selection_sort(array: list) -> list:
+    for i in range(len(array)):
+        lowest = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[lowest]:
+                lowest = j
+        array[i], array[lowest] = array[lowest], array[i]
     return array
 
 
